f71f0b891ab87701797dfb709806d869630c16d9
roronoacheick <roronoacheick3@icloud.com>
2026-02-16 21:17:10 +0100

    feature: Loading data

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.loader import load_json
+
+
+warehouse = load_json("data/warehouse.json")
+agents = load_json("data/agents.json")
+orders = load_json("data/orders.json")
+products = load_json("data/products.json")
